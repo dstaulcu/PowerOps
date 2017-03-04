c5a6914577a976c49858ca9c2b94796113e20ff1
dstaulcu <David_Staulcup@hotmail.com>
2017-03-04 11:39:45 -0500

    changed harness to execute specified script in catalog folder

--- ExecuteParallel.ps1
+++ ExecuteParallel.ps1
@@ -6,20 +6,7 @@
 
     foreach -parallel ($Computer in $Computers) {
 
-        InlineScript {
-
-            function Print-FomattedEvent {
-                param (
-                    [string[]] $Message
-                )
-                $EventTime = Get-Date -format "yyyy-MM-dd HH:mm:ss.fff"
-                "$EventTime;$env:computername;$Message"
-            }
-
-            Print-FomattedEvent("Hello world!")
-
-
-        } -PSComputerName $Computer -PSCredential $Credential
+        InlineScript { C:\Users\David\Documents\Development\projects\PowerOps\catalog\helloworld.ps1 } -PSComputerName $Computer -PSCredential $Credential
     }
 }
 
