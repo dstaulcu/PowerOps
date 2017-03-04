4f044e7598e965e768300ffdc8fe738d1267d3e0
dstaulcu <David_Staulcup@hotmail.com>
2017-03-04 15:35:01 -0500

    catalog scripts

---
+++ catalog/helloworld.ps1
@@ -0,0 +1,9 @@
+﻿function Print-FomattedEvent {
+    param (
+        [string[]] $Message
+    )
+    $EventTime = Get-Date -format "yyyy-MM-dd HH:mm:ss.fff"
+    "$EventTime;$env:computername;$Message"
+}
+
+Print-FomattedEvent("Hello world!")
