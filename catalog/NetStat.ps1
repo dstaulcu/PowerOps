638e9174f998e47ae771486ebc8e0768e1fac060
dstaulcu <David_Staulcup@hotmail.com>
2017-04-25 07:14:15 -0400

    more catalog scripts

---
+++ catalog/NetStat.ps1
@@ -0,0 +1,40 @@
+﻿function Get-NetConnections
+{
+    $a = netstat -p tcp -n  
+    $a = $a[4..$a.count] | ConvertFrom-String | select p2,p3,p4,p5
+    $connections = @()
+    foreach ($entry in $a)
+    {
+        $Proto = $entry.p2
+        $LocalAddress = ($entry.P3).Split(":")[0]
+        $LocalPort = ($entry.P3).Split(":")[1]
+        $RemoteAddress = ($entry.P4).Split(":")[0]
+        $RemotePort = ($entry.P4).Split(":")[1]
+        $State = $entry.p5
+
+        $CustomEvent = New-Object -TypeName PSObject                 
+        $CustomEvent | Add-Member -Type NoteProperty -Name "Proto" -Value $Proto
+        $CustomEvent | Add-Member -Type NoteProperty -Name "LocalAddress" -Value $LocalAddress
+        $CustomEvent | Add-Member -Type NoteProperty -Name "LocalPort" -Value $LocalPort
+        $CustomEvent | Add-Member -Type NoteProperty -Name "RemoteAddress" -Value $RemoteAddress
+        $CustomEvent | Add-Member -Type NoteProperty -Name "RemotePort" -Value $RemotePort
+        $CustomEvent | Add-Member -Type NoteProperty -Name "State" -Value $State
+        $Connections += $CustomEvent
+    }
+    return $connections
+}
+
+# get current tcp-based network connections
+$connections = Get-NetConnections
+
+# get the distinct remoteaddresses amid connections
+$RemoteAddresses = $connections | Select-Object -Unique RemoteAddress -ExpandProperty RemoteAddress
+
+
+
+<#
+foreach ($RemoteAddress in $RemoteAddresses)
+{
+    write-host $RemoteAddress
+}
+#>
---
+++ catalog/set-splunk-clone-config.ps1
@@ -0,0 +1,47 @@
+﻿function Print-FomattedEvent {
+    param (
+        [string[]] $Message
+    )
+    $EventTime = ((get-date).ToUniversalTime()).ToString("yyyy-MM-dd HH:mm:ss.fff")
+    "$EventTime;$env:computername;$Message"
+}
+
+function CheckKeyValue($file, $keyname, $expectedvalue)
+{
+    $fileinfo = Get-Item $file
+    $data = Get-Content $file
+    $match = $true
+    foreach ($line in $data)
+    {
+        if ($line -like "$keyname*")
+        {
+            $pair = $line.split("=")
+            $key = $pair[0].trim()
+            $value = $pair[1].trim()
+#            Print-FomattedEvent("key $($keyname) from $($fileinfo.Name); contains $($value)")
+            if ($value -ne $expectedvalue)
+            {
+                $match = $false
+            }
+        }
+    }
+    return $match
+}
+
+$file = "C:\Program Files\SplunkUniversalForwarder\etc\system\local\inputs.conf"
+if (test-path -Path $file) { $check1 = CheckKeyValue -file $file -keyname "host" -expectedvalue $env:COMPUTERNAME }
+
+$file = "C:\Program Files\SplunkUniversalForwarder\etc\system\local\server.conf"
+if (test-path -path $file) { $check2 = CheckKeyValue -file $file -keyname "serverName" -expectedvalue $env:computername }
+
+if ((!($check1)) -or (!($check2)))
+{
+    Print-FomattedEvent("agent identity is non-compliant, correcting issue.")
+    Start-Process -FilePath "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" -argumentlist "stop" -Wait -WindowStyle Hidden
+    & 'C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe' clone-prep-clear-config
+    Start-Process -FilePath "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" -argumentlist "start" -Wait -WindowStyle Hidden
+}
+else
+{
+   Print-FomattedEvent("agent identity is compliant.")
+}
