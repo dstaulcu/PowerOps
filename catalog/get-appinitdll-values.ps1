dcb42771d3cc4097db68262c5859ad2feff6e763
dstaulcu <dstaulcu@users.noreply.github.com>
2017-11-04 15:54:02 -0400

    Add files via upload

---
+++ catalog/get-appinitdll-values.ps1
@@ -0,0 +1,26 @@
+﻿function Print-FormattedEvent {
+    param (
+        [string[]] $Message
+    )
+    $EventTime = ((get-date).ToUniversalTime()).ToString("yyyy-MM-dd HH:mm:ss.fff")
+    "$EventTime;$env:computername;$Message"
+}
+
+
+$Key = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Windows'
+$ValueName = 'AppInit_DLLs'
+$ValueObj = (Get-ItemProperty -Path $Key -Name $ValueName -ErrorAction SilentlyContinue) 
+if (!($ValueObj)) {
+    Print-FormattedEvent("$($ValueObj) object was not present in $($Key).")
+} else {
+    $ValueData = $ValueObj | Select-Object -ExpandProperty $ValueName
+    if (!($ValueData)) {
+        Print-FormattedEvent("$($ValueName) contained not data items.")
+    } else {
+        Print-FormattedEvent("$($ValueName) contained value data $($valuedata).")
+    }
+ 
+}
+
+
+
---
+++ catalog/set-appinitdlls-null-ps1.ps1
@@ -0,0 +1,29 @@
+﻿function Print-FormattedEvent {
+    param (
+        [string[]] $Message
+    )
+    $EventTime = ((get-date).ToUniversalTime()).ToString("yyyy-MM-dd HH:mm:ss.fff")
+    "$EventTime;$env:computername;$Message"
+}
+
+
+$Key = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Windows'
+$ValueName = 'AppInit_DLLs'
+$ValueObj = (Get-ItemProperty -Path $Key -Name $ValueName -ErrorAction SilentlyContinue) 
+if (!($ValueObj)) {
+    Print-FormattedEvent("$($ValueObj) object was not present in $($Key).")
+} else {
+    $ValueData = $ValueObj | Select-Object -ExpandProperty $ValueName
+    if (!($ValueData)) {
+        $oldvaluedata = ''
+    } else {
+        $oldvaluedata = $valuedata
+    }
+
+    New-ItemProperty -Path $Key -name $ValueName -PropertyType String -Value $newvalue -ErrorAction SilentlyContinue
+    Print-FormattedEvent("$($ValueName) changed from [$($oldvalue)] to [$($newvalue)].")
+ 
+}
+
+
+
